a00ed4eabfd4af3d2c21e300e47abf3a87f8d5cf
Cyril- <1978919203@qq.com>
2022-03-29 11:58:43 +0800

    ds

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-05 08:51:21
+ * @LastEditTime: 2022-03-29 11:57:05
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /admin/src/App.test.js
+ */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,3 +14,6 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// 测试使用
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-05 08:51:21
+ * @LastEditTime: 2022-03-29 11:56:05
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /admin/src/setupTests.js
+ */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// 组件测试
